be2b02631e3b0948f3027ac8d2c11208441ac0a1
SystemPLA <amalovatyj@yandex.ru>
2020-04-29 14:26:36 +0300

    libraries

---
+++ app/src/main/java/ru/systempla/life_hack_studio_test/App.kt
@@ -0,0 +1,7 @@
+package ru.systempla.life_hack_studio_test
+
+import android.app.Application
+
+class App : Application() {
+
+}
---
+++ app/src/main/java/ru/systempla/life_hack_studio_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.systempla.life_hack_studio_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
